edf27e5b22d35f9903fdb08d502fced1a9edca99
Michael Votaw <mike@thinkhappymedium.com>
2020-01-22 16:52:51 -0600

    Setup Notification handling in HomeViewController for setting up/tearing down Tap and Display controllers.

--- LaTAR iOS/LaTAR iOS/HomeViewController.swift
+++ LaTAR iOS/LaTAR iOS/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var versionLabel: UILabel!
     @IBOutlet weak var textLog: UITextView!
     
-    
+    public var logText:String = "";
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,44 +21,73 @@ class HomeViewController: UIViewController {
         
         self.versionLabel.text = "Ver \(version)";
         
+        NotificationCenter.default.addObserver(self, selector: #selector(displayLog(notification:)),
+                                               name: LALogging.loggedNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(setupTapLatency(notification:)),
+                                               name: tapLatenceySetupNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(teardownTapLatency(notification:)),
+                                               name: tapLatenceyTeardownNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(setupDisplayLatency(notification:)),
+                                               name: displayLatenceySetupNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(teardownDisplayLatency(notification:)),
+                                               name: displayLatenceyTeardownNotification, object: nil);
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated);
-        NotificationCenter.default.addObserver(self, selector: #selector(displayLog(notification:)), name: LALogging.loggedNotification, object: nil);
-        NotificationCenter.default.addObserver(self, selector: #selector(setupTapLatency(notification:)), name: tapLatenceySetupNotification, object: nil);
-        
-        LaTARSocket.initializeShared();
-
     }
-    
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated);
-        NotificationCenter.default.removeObserver(self);
-    }
-
     
     @objc func setupTapLatency(notification:Notification)
     {
-        self.performSegue(withIdentifier: "showTapLatency", sender: nil);
+        
+        let vc = TapLatencyViewController.init(nibName: "TapLatencyViewController", bundle: nil);
+        
+        vc.modalPresentationStyle = .fullScreen;
+        self.present(vc, animated: true) {
+            LaTARSocket.shared.acknowledgeCommand(.TAP_SETUP);
+        }
+    }
+    
+    
+    @objc func setupDisplayLatency(notification:Notification)
+    {
+        
+        let vc = DisplayLatencyViewController.init(nibName: "DisplayLatencyViewController", bundle: nil);
+        
+        vc.modalPresentationStyle = .fullScreen;
+        self.present(vc, animated: true) {
+            LaTARSocket.shared.acknowledgeCommand(.DISPLAY_SETUP);
+        }
+    }
+    
+    
+    @objc func teardownTapLatency(notification:Notification)
+    {
+        self.dismiss(animated: true) {
+            LaTARSocket.shared.acknowledgeCommand(.TAP_TEARDOWN);
+        }
+    }
+    
+    
+    @objc func teardownDisplayLatency(notification:Notification)
+    {
+        
+        self.dismiss(animated: true) {
+            LaTARSocket.shared.acknowledgeCommand(.DISPLAY_TEARDOWN);
+        }
     }
     
 
     @objc func displayLog(notification:Notification)
     {
         guard let log:String = notification.userInfo?["log"] as? String else { return;}
+        self.logText += log + "\n";
         DispatchQueue.main.async{
-            self.textLog.text += log + "\n";
+            if !(self.viewIfLoaded?.isHidden ?? true)
+            {
+                self.textLog.text += log + "\n";
+            }
         }
-    }
-    
-    
-    @IBSegueAction func showTapLatency(_ coder: NSCoder) -> TapLatencyViewController? {
-        let vc = TapLatencyViewController(coder: coder);
-        vc?.modalPresentationStyle = .fullScreen;
-        
-        return vc;
     }
     
 }
